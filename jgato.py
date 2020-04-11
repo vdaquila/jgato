72d1c33e1b7753af9b6d81060ac629d78504b0d1
Kevin D'Aquila <kevin@kdaq.net>
2020-04-11 15:44:06 +0100

    Initial functional category query

--- jgato.py
+++ jgato.py
@@ -1,56 +1,68 @@
 #!/usr/bin/python3
 
 import flask
+import json
+import psycopg2
 
 app = flask.Flask(__name__)
+conn = psycopg2.connect("dbname=jgato user=postgres")
 
 @app.route('/')
-def index():
-    return "Index!"
+def category_picker():
+    """
+    URL: /
 
-@app.route("/clues")
-def clues():
-    """
-    /Clues
-    Url: /api/clues
-    Options All options are optional:
-    value(int): the value of the clue in dollars
-    category(int): the id of the category you want to return
-    min_date(date): earliest date to show, based on original air date
-    max_date(date): latest date to show, based on original air date
-    offset(int): offsets the returned clues. Useful in pagination
-    """
-    return "Clues!"
+    Initial screen to pick categories.
 
-@app.route("/random")
-def random():
-    """
-    /Random
-    Url: /api/random
     Options:
-    count(int): amount of clues to return, limited to 100 at a time
+    ...
+    
     """
-    return "Random!"
 
-@app.route("/categories")
-def categories():
-    """
-    /Categories
-    Url: /api/categories
-    Options:
-    count(int): amount of categories to return, limited to 100 at a time
-    offset(int): offsets the starting id of categories returned. Useful in pagination.
-    """
-    return "Categories!"
+    key_query_map = {
+        "jeopardy_round": 
+            "SELECT DISTINCT cat.id as id, cl.game_id as show_number, cat.title as name, cl.airdate "
+            "FROM clues AS cl, categories AS cat "
+            "WHERE cat.id = cl.category_id AND (cl.value = 200 OR cl.value = 600);",
+        "double_jeopardy_round": 
+            "SELECT DISTINCT cat.id as id, cl.game_id as show_number, cat.title as name, cl.airdate "
+            "FROM clues AS cl, categories AS cat "
+            "WHERE cat.id = cl.category_id and cl.value > 1000;",
+        "final_jeopardy_round": 
+            "SELECT DISTINCT cat.id as id, cl.game_id as show_number, cat.title as name, cl.airdate "
+            "FROM clues AS cl, categories AS cat "
+            "WHERE cat.id = cl.category_id and cl.value = 0;",
+    }
+    result_d = {k: {"categories": []} for k in key_query_map.keys()}
 
-@app.route("/category")
-def category():
+    cur = conn.cursor()
+    for round_key, round_query in key_query_map.items():
+        cur.execute(round_query)
+        row = cur.fetchone()
+        while row is not None:
+            category_d = {
+                "id": row[0],
+                "show_number": row[2],
+                "name": row[2],
+                "airdate": str(row[3]),
+            }
+            result_d[round_key]["categories"].append(category_d)
+            row = cur.fetchone()
+
+    return(json.dumps(result_d))
+
+@app.route("/play")
+def game_board():
     """
-    /Category
-    Url: /api/category
+    URL: /play
+
+    With categories picked, send back full game data.
+
     Options:
     id(int): Required the ID of the category to return.
     """
+
+    # TODO update description
     return "Category!"
 
 
