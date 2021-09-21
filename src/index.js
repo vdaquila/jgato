ad5d8796ae6d10446a66bb50f1a22c664d8cac26
Val D'Aquila <val.daquila@gmail.com>
2021-09-20 21:31:54 -0400

    organization, node-sass, convert CSS to SASS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Game from './components/Game';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));
 
